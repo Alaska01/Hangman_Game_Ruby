cd30ca70eac6aa9f43ea45a0105cc9308dd4fe1c
Alaska01 <aadaniel108@gmail.com>
2020-08-03 20:43:09 +0100

    Rules for Hangmans game listed

---
+++ play.rb
@@ -0,0 +1,8 @@
+# A game built in Ruby, played on terminal.
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# RULES
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# Pick Letters and try to complete a word guess
+# Too many mistakes, game is over, because you have ran out of lives
