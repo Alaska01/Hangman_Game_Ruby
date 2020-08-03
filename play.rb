f0bf13e86e7c69151931e8a520be4fb87fbfba13
Alaska01 <aadaniel108@gmail.com>
2020-08-03 20:45:35 +0100

    created the Hangman Class

--- play.rb
+++ play.rb
@@ -6,3 +6,18 @@
 
 # Pick Letters and try to complete a word guess
 # Too many mistakes, game is over, because you have ran out of lives
+
+class Hangman
+
+  def initialize
+  end
+
+  def begin
+    
+  # Ask user to begin the game
+    
+  end
+
+
+
+end
